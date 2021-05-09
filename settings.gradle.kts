16f1bfd2bd9ad4dc2724c301ec960ba859c0f866
Darien Imai <darien@dpsi.jp>
2021-05-09 11:43:49 -0700

    threereeds branding.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
     }
 }
 
-val forkName = "MyAirplaneFork"
+val forkName = "threereeds"
 val forkNameLowercase = forkName.toLowerCase(Locale.ENGLISH)
 
 rootProject.name = forkNameLowercase
